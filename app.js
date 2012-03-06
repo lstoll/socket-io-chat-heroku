e1b24d8c980e7269bd1e2ebef4a5c676f6aceb36
Lincoln Stoll <lstoll@lstoll.net>
2012-03-06 16:38:32 +1100

    set port for heroku

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.get('/', function (req, res) {
  * App listen.
  */
 
-app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
   var addr = app.address();
   console.log('   app listening on http://' + addr.address + ':' + addr.port);
 });
